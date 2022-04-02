b97c40974bf9a9b54033bbb05ca2ba1afec7fa7f
Zhu Allen <allenz@M1-Mac-Allen.local>
2022-04-02 18:23:57 -0500

    new change

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 
-ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<Hello/>);
+// ReactDOM.render(
+//   <Hello />,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
